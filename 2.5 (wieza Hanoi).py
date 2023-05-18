ec53490ffdd9662d6bae1332e139dbfa3b0043ae
broniewskaK <broniewska.k@gmail.com>
2023-05-18 15:06:59 +0200

    lab 2 zadanie 5 (wieża Hanoi)

--- 2.5 (wieza Hanoi).py
+++ 2.5 (wieza Hanoi).py
@@ -2,6 +2,6 @@ def hanoi(n,A,B,C):
     if n > 0:
         hanoi(n-1,A,C,B)
         print(f"Przenies dysk {n} z {A} na {C}")
-        hanoi(n-1,B,A,C)
+        hanoi(n-1,C,B,A)
 n=int(input('podaj liczbę krążków'))
 hanoi(n, "A", "B", "C")
